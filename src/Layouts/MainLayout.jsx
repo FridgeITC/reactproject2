563b54fb3530adf10ed384bf70bd9008f5a98449
InigoZepeda <inigis@hotmail.com>
2022-11-24 21:21:36 -0600

    navbar complete 2

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -12,12 +12,8 @@ import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import SettingsIcon from '@mui/icons-material/Settings';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 
 import ControlledAccordions from '../Components/AccordionNav';
 
@@ -116,15 +112,34 @@ export default function PersistentDrawerLeft() {
         anchor='left'
         open={open}
       >
-        <DrawerHeader>
-          <p style={{textAlign: 'left', width: '100%', paddingLeft: '1rem'}}>¡Bienvenido, Iñigo!</p>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-          </IconButton>
-        </DrawerHeader>
-
-        <Divider />
-        <ControlledAccordions />
+        <div>
+          <DrawerHeader>
+            <p style={{textAlign: 'left', width: '100%', paddingLeft: '1rem'}}>¡Bienvenido, Iñigo!</p>
+            <IconButton onClick={handleDrawerClose}>
+              {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+            </IconButton>
+          
+          </DrawerHeader>
+          <Divider />
+          <ControlledAccordions />
+        </div>
+        <div style={{
+          paddingLeft: '1rem',
+        }}>
+        <button className='button-nav' style={{paddingTop: '1rem', paddingBottom: '1rem', display: 'flex', justifyContent: 'space-between', width: '100%'}}>
+          <Typography>
+              Editar locales
+            </Typography>
+            <SettingsIcon />
+          </button>
+          <Divider />
+          <button className='button-nav' style={{paddingTop: '1rem', paddingBottom: '1rem', display: 'flex', justifyContent: 'space-between', width: '100%'}}>
+          <Typography>
+              Agregar local
+            </Typography>
+            <AddCircleOutlineIcon />
+          </button>
+        </div>
       </Drawer>
       {/* Sidebar Ends */}
       <Main open={open}>
