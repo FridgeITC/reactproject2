7f766f19648ff33dd25cae4a1e8af93d24389d00
InigoZepeda <inigis@hotmail.com>
2022-11-25 09:49:06 -0600

    added table locales

---
+++ src/Components/TableLocales.jsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+function createData(local, zona, eliminar, editar) {
+  return {local, zona, eliminar, editar};
+}
+
+const rows = [
+  createData('Oxxo 1', 'Zona Sur', 'Eliminar', 'Editar'),
+  createData('Oxxo 2', 'Zona Norte', 'Eliminar', 'Editar'),
+  createData('Oxxo 3', 'Zona Norte', 'Eliminar', 'Editar'),
+];
+
+export default function BasicTable() {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650, width: '60%' }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Local</TableCell>
+            <TableCell align="right">Zona</TableCell>
+            <TableCell align="right">Acción</TableCell>
+            <TableCell align="right">Edición</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.local}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.local}
+              </TableCell>
+              <TableCell align="right">{row.zona}</TableCell>
+              <TableCell align="right">{row.eliminar}</TableCell>
+              <TableCell align="right">{row.editar}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -16,6 +16,7 @@ import SettingsIcon from '@mui/icons-material/Settings';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 
 import ControlledAccordions from '../Components/AccordionNav';
+import BasicTable from '../Components/TableLocales';
 
 
 import "../Assets/Styles/base.css";
@@ -144,16 +145,10 @@ export default function PersistentDrawerLeft() {
       {/* Sidebar Ends */}
       <Main open={open}>
         <DrawerHeader />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Rhoncus dolor purus non enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-          imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus. Convallis convallis tellus id interdum
-          velit laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate eu
-          scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt
-          lobortis feugiat vivamus at augue. At augue eget arcu dictum varius duis at consectetur lorem. Velit sed
-          ullamcorper morbi tincidunt. Lorem donec massa sapien faucibus et molestie ac.
-        </Typography>
+        <div style={{width: '90vw', margin: 'auto'}}>
+          <BasicTable />
+
+        </div>
         
       </Main>
     </Box>
