fef3be91f90c448c5378faa312afe1e49e567c42
InigoZepeda <inigis@hotmail.com>
2022-11-30 10:15:40 -0600

    Table show locales

--- src/Components/ModalAgregaLocal.jsx
+++ src/Components/ModalAgregaLocal.jsx
@@ -13,17 +13,31 @@ import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
 import Switch from '@mui/material/Switch';
 import TextField from '@mui/material/TextField';
+import { useParams } from 'react-router-dom';
 
-export default function ModalAgregaLocal() {
+import axios from '../Config/axios';
+
+export default function ModalAgregaLocal({refresh, setRefresh}) {
   const [open, setOpen] = React.useState(false);
   const [fullWidth, setFullWidth] = React.useState(true);
   const [maxWidth, setMaxWidth] = React.useState('sm');
+
+  const params = useParams();
+
+  const [newLocal, setNewLocal] = React.useState({ local: params.id });
 
   const handleClickOpen = () => {
     setOpen(true);
   };
 
-  const handleClose = () => {
+  const handleClose = (action) => {
+    if(action === 'confirm') {
+      axios.post('/local/add', newLocal)
+      .then((res) => {
+        setRefresh(refresh+1);
+      })
+      .catch(error => {console.log(error)})
+      }
     setOpen(false);
   };
 
@@ -32,11 +46,16 @@ export default function ModalAgregaLocal() {
       // @ts-expect-error autofill of arbitrary value is not handled.
       event.target.value,
     );
+    setNewLocal({...newLocal, zone: event.target.value});
   };
 
   const handleFullWidthChange = (event) => {
     setFullWidth(event.target.checked);
   };
+
+  const handleNameChange = (e) => {
+    setNewLocal({...newLocal, name: e.target.value});
+  }
 
   return (
     <React.Fragment>
@@ -64,6 +83,7 @@ export default function ModalAgregaLocal() {
             <FormControl sx={{ mt: 2, minWidth: 120 }}>
               <InputLabel htmlFor="max-width">Zona</InputLabel>
               <Select
+              name="zone"
                 autoFocus
                 value={maxWidth}
                 onChange={handleMaxWidthChange}
@@ -73,16 +93,17 @@ export default function ModalAgregaLocal() {
                   id: 'max-width',
                 }}
               >
-                <MenuItem value="norte">Zona Norte</MenuItem>
-                <MenuItem value="sur">Zona Sur</MenuItem>
-                <MenuItem value="oriente">Zona Oriente</MenuItem>
-                <MenuItem value="poniente">Zona Poniente</MenuItem>
+                <MenuItem value="3">Zona Norte</MenuItem>
+                <MenuItem value="2">Zona Sur</MenuItem>
+                <MenuItem value="5">Zona Oriente</MenuItem>
+                <MenuItem value="6">Zona Poniente</MenuItem>
               </Select>
             </FormControl>
           </Box>
           <TextField
             autoFocus
             margin="dense"
+            name="name_local"
             id="name_local"
             label="Nombre del local"
             type="text"
@@ -93,11 +114,12 @@ export default function ModalAgregaLocal() {
                 m: 'auto',
                 width: '50%',
               }}
+              onChange={handleNameChange}
           />
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose}>Cancelar</Button>
-          <Button onClick={handleClose}>Confirmar</Button>
+          <Button onClick={() => handleClose('cancel')}>Cancelar</Button>
+          <Button onClick={() => handleClose('confirm')}>Confirmar</Button>
         </DialogActions>
       </Dialog>
     </React.Fragment>
